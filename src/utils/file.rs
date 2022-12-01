af3f7dff6c25ad47911b6114aac549e707ed09fa
Richard Shephard <richard.shephard@gmail.com>
2022-12-01 11:30:48 +0200

    add some utils

---
+++ src/utils/file.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs;
+
+pub fn read_input(day: String) -> String {
+    let file_path = env::current_dir()
+        .unwrap()
+        .join("src")
+        .join(day)
+        .join("input");
+
+    match fs::read_to_string(&file_path) {
+        Ok(input) => input,
+        Err(error) => panic!("Failed to read file ({}): {:?}", file_path.display(), error)
+    }
+}

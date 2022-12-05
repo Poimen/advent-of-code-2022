b536b36d43067c11d93d9ee73a4c94febf2f8562
Richard Shephard <richard.shephard@gmail.com>
2022-12-05 09:39:15 +0200

    day 2 answers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod day1;
+pub mod day2;
 pub mod utils;
 
 fn main() {
@@ -6,8 +7,14 @@ fn main() {
 }
 
 fn solve_day_1() {
-    let res = utils::file::read_input("day1".to_string());
+    let mut res = utils::file::read_input("day1".to_string());
 
     println!("Max calaries carried by any elf: {}", day1::solve_part_1(&res));
     println!("Max calaries carried by top 3 elves: {}", day1::solve_part_2(&res));
+
+
+    res = utils::file::read_input("day2".to_string());
+
+    println!("Elf strategy guide score: {}", day2::solve_part_1(&res));
+    println!("Elf win/lose strategy guide score: {}", day2::solve_part_2(&res));
 }
